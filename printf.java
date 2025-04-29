972de65f7d104a255c769cc0b5e9f001240969ac
Kunal sharma <143254152+kunal8s@users.noreply.github.com>
2025-04-29 20:40:14 +0530

    Create printf.java

---
+++ printf.java
@@ -0,0 +1,25 @@
+// printf in java
+
+public class Main{
+    public static void main(String[] args) {
+        String name ="kunal";
+        char first_letter = 'K';
+        int Age = 21;
+        double height = 175.0;
+        boolean is_employed = true;
+
+        
+        System.out.printf("My name is %s\n",name);
+        System.out.printf("I am %d years old.\n",Age);
+        System.out.printf("My name first letter is %c.\n", first_letter);
+        System.out.printf("My height is %f cm\n", height);
+        System.out.printf("Am i employed: %b\n", is_employed);
+
+        System.out.printf("Hey I am %s , I am %d years old.", name, Age);
+         
+        if float is so long if we want only two digit %.2f
+
+            zero and normal padding study from screenshot
+
+    }
+}
