dc39ae234dfca404cb51e30f0eadad45fb972633
Kunal sharma <143254152+kunal8s@users.noreply.github.com>
2025-04-29 20:34:08 +0530

    Create variables.java

---
+++ variables.java
@@ -0,0 +1,17 @@
+// public class Main{
+//     public static void main(String[] args) {
+//         String name = "Kunal";
+//         int age = 21;
+//         double gpa = 7.5;
+//         boolean iseligible = true;
+//         char grade = 'A';
+//         System.out.println("My Name is: " + name);
+//         System.out.println("My Age is: " + age);
+//         System.out.println("My GPA is: " + gpa);
+//         System.out.println("I am Eligible: " + iseligible);
+//         System.out.println("My Grade is: " + grade);
+//     }
+// } //variable
+
+
+example of variable in java 
